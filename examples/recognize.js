41826753975c41af544dbf5ce5a838819df02718
taoyuan <torworx@gmail.com>
2016-03-31 19:10:29 +0800

    Fix language

--- examples/recognize.js
+++ examples/recognize.js
@@ -11,7 +11,7 @@ var tess = tessocr.tess();
 var fixture = fixtures[2];
 
 console.time('recognize');
-tess.recognize(fixture.image, {lang: fixture.lang, psm: 6}, function (err, result) {
+tess.recognize(fixture.image, {language: fixture.lang, psm: 6}, function (err, result) {
   if (err) throw err;
   console.log(result);
   console.timeEnd('recognize');
